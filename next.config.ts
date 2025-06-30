d1f4c63393e18c18141c6cba8aae82395832bad1
shashank-padala <shashank.padala24@gmail.com>
2025-06-30 15:21:29 +0530

    added basic auth funtionality

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Your existing config options here
+};
+
+export const config = {
+  matcher: ["/"], // apply middleware to homepage
 };
 
 export default nextConfig;
